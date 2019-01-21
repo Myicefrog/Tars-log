f8b38934ee3ac90d7bc9ac51316583c736f7788e
Myicefrog <490925498@qq.com>
2019-01-20 16:51:22 -0800

    change log path

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ using namespace tars;
 
 int main()
 {
-	TarsRollLogger::getInstance()->setLogInfo("Hello", "HelloServer", "/home/luguang/demo/tars-log/", 52428800, 10, "tars.hellolog.LogObj");
+	TarsRollLogger::getInstance()->setLogInfo("Hello", "HelloServer", ".", 52428800, 10, "tars.hellolog.LogObj");
 
 	TarsRollLogger::getInstance()->logger()->setLogLevel("DEBUG");
 
